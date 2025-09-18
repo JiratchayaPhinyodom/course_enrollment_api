aafb31a9b8ed9adaab9dde58c9d2b65843b37306
JiratchayaPhinyodom <jiratchayaphinyodom@JiratchayaPhinyodoms-MacBook-Pro.local>
2025-09-19 04:34:22 +0700

    Updates apps

--- course_enrollment_api/apps.py
+++ course_enrollment_api/apps.py
@@ -15,18 +15,10 @@ class CourseEnrollmentApiConfig(AppConfig):
 
     plugin_app = {
         PluginURLs.CONFIG: {
-            # Hook into LMS
             "lms.djangoapp": {
                 PluginURLs.NAMESPACE: "course_enrollment_api",
-                PluginURLs.REGEX: r"^api/enrollment/",
-                PluginURLs.RELATIVE_PATH: "urls",
-            },
-            # Hook into CMS if you want (optional)
-            "cms.djangoapp": {
-                PluginURLs.NAMESPACE: "course_enrollment_api",
-                PluginURLs.REGEX: r"^api/enrollment/",
+                PluginURLs.REGEX: r"^api/enrollment/course/",
                 PluginURLs.RELATIVE_PATH: "urls",
             },
         },
-        # (Optional: You can add PluginSettings or PluginSignals later if needed)
     }
