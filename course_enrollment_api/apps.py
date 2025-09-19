fcfe662eb09d81c596657b3ff6d3729691ce6fbe
JiratchayaPhinyodom <jiratchayaphinyodom@JiratchayaPhinyodoms-MacBook-Pro.local>
2025-09-19 07:01:04 +0700

    Updates apps

--- course_enrollment_api/apps.py
+++ course_enrollment_api/apps.py
@@ -12,12 +12,12 @@ class CourseEnrollmentApiConfig(AppConfig):
         PluginURLs.CONFIG: {
             "lms.djangoapp": {
                 PluginURLs.NAMESPACE: "course_enrollment_api",
-                PluginURLs.REGEX: r"^plugin/enrollment-api/",
+                PluginURLs.REGEX: r"^api/enrollment-api/",
                 PluginURLs.RELATIVE_PATH: "urls",
             },
             "cms.djangoapp": {
                 PluginURLs.NAMESPACE: "course_enrollment_api",
-                PluginURLs.REGEX: r"^plugin/enrollment-api/",
+                PluginURLs.REGEX: r"^api/enrollment-api/",
                 PluginURLs.RELATIVE_PATH: "urls",
             },
         },
